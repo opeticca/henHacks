f260f39f2a68083459197e2d8ab7a01d97440340
Michelle Ratanraj <michelleratanraj2007@gmail.com>
2026-02-28 14:58:44 -0500

    work in progress

---
+++ src/parse.js
@@ -0,0 +1,18 @@
+
+var inputDate=prompt("Enter the date ");
+var inputTime=prompt("Enter the time ");
+var inputIsHot=true;
+
+
+
+
+
+//JSON.parse
+let dictionary1 = {
+    "Date": inputDate,
+    "Time": "x",
+    "isHot": truein
+}
+
+
+console.log(dictionary1);
